cc3c757c03922eb521828f7519cd6b44d3d81b61
Emily Bolten <emilybolten@gmail.com>
2019-04-08 17:11:26 +0000

    Automatically backed up by Learn

--- test/root.js
+++ test/root.js
@@ -25,3 +25,32 @@ before(function(done) {
     return done();
   });
 });
+
+function takeANumber(currentLine,name) {
+    //add first element of array
+    currentLine.push(name);
+    //returning a string printing number and person in line
+    return console.log(`"Welcome ${name}, you are number ${currentLine.length} in line."`);
+}
+
+function nowServing(currentLine) {
+  //if the line has nobody, return string
+  if (currentLine.length == 0) {
+    return console.log("There is nobody waiting to be served!");
+  }
+  //if line has people, print who is being served
+  else {
+    console.log(`"Currently serving ${currentLine[0]}."`);
+    //delete beginning element of array
+    currentLine.shift();
+    return currentLine;
+  }
+}
+
+function currentLine (currentLine) {
+    //use for loop to print each person in line
+    console.log("This line is currently: ");
+    for (i = 0; i < currentLine.length; i++) {
+        console.log(`"${i+1}. ${currentLine[i]}"`);
+    }
+}
