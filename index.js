afb5a3415c9561c6ca8f3dfdd96ed768781eb320
Emily Bolten <emilybolten@gmail.com>
2019-04-08 17:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function takeANumber(currentLine,name) {
     //returning a string printing number and person in line
     return console.log(`"Welcome ${name}, you are number ${currentLine.length} in line."`);
 }
+
+function nowServing(currentLine) {
+  //if the line has nobody, return string
+  if (currentLine.length == 0) {
+    return console.log("There is nobody waiting to be served!");
+  }
+  //if line has people, print who is being served
+  else {
+    console.log(`"Currently serving ${currentLine[0]}."`);
+    //delete beginning element of array
+    currentLine.shift();
+    return currentLine;
+  }
+}
+
